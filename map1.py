9754a98d72c067e04ccf1f9639f30caf31498b58
Ewelina Backtrog <oyfg@o2.pl>
2019-05-22 11:22:45 +0200

    create a feature group
    #when we add a layer control feature it allows to switch other layers on 
    and off

--- map1.py
+++ map1.py
@@ -1,6 +1,8 @@
 import folium
 map = folium.Map(location=[64.14, -21.94], zoom_start=6, tiles="Mapbox Bright")
 
-map.add_child(folium.Marker(location=[64.14, -21.93], popup="I am a Marker", icon=folium.Icon(color="pink")))
+fg = folium.FeatureGroup(name="My Map")
+fg.add_child(folium.Marker(location=[64.14, -21.93], popup="I am a Marker", icon=folium.Icon(color="pink")))
+map.add_child(fg)
 
 map.save("Map1.html")
