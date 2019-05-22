7cbb2916091e9fe4c531b746c6a2d6328faea646
Ewelina Backtrog <oyfg@o2.pl>
2019-05-22 15:01:41 +0200

    change popup frame

--- map1.py
+++ map1.py
@@ -6,11 +6,16 @@ lon = list(data["LON"])
 lat = list(data["LAT"])
 name = list(data["NAME"])
 
+html = """<h4>Volcano name:</h4>
+%s
+"""
+
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Mapbox Bright")
 
 fg = folium.FeatureGroup(name="My Map")
 for lt, ln, nm in zip (lat, lon, name):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=nm, icon=folium.Icon(color="pink")))
+    iframe = folium.IFrame(html=html % nm, width=200, height=100)
+    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color="red")))
 
 
 map.add_child(fg)
