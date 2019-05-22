c2d63ae9573efa005e19d54447609dbad9c72bd3
Ewelina Backtrog <oyfg@o2.pl>
2019-05-22 13:06:10 +0200

    add Volcanoes.txt and American Volcano Markers

--- map1.py
+++ map1.py
@@ -1,9 +1,16 @@
 import folium
-map = folium.Map(location=[64.14, -21.94], zoom_start=6, tiles="Mapbox Bright")
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+lon = list(data["LON"])
+lat = list(data["LAT"])
+name = list(data["NAME"])
+
+map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Mapbox Bright")
 
 fg = folium.FeatureGroup(name="My Map")
-for coordinates in [[64.14, -21.93], [64.34, -21.73], [64.54, -21.53], [64.74, -21.33]]:
-    fg.add_child(folium.Marker(location=coordinates, popup="I am a Marker", icon=folium.Icon(color="pink")))
+for lt, ln, nm in zip (lat, lon, name):
+    fg.add_child(folium.Marker(location=[lt, ln], popup=[nm], icon=folium.Icon(color="pink")))
 
 map.add_child(fg)
 
