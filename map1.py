56e28a8c4d448859b96ac528b801a94e2a0a0c2a
Ewelina Backtrog <oyfg@o2.pl>
2019-05-22 11:52:01 +0200

    add marker-points with for loop method

--- map1.py
+++ map1.py
@@ -2,10 +2,9 @@ import folium
 map = folium.Map(location=[64.14, -21.94], zoom_start=6, tiles="Mapbox Bright")
 
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location=[64.14, -21.93], popup="I am a Marker-kun", icon=folium.Icon(color="pink")))
-fg.add_child(folium.Marker(location=[64.34, -21.73], popup="I am a Marker-chan", icon=folium.Icon(color="pink")))
-fg.add_child(folium.Marker(location=[64.54, -21.53], popup="I am a Marker-san", icon=folium.Icon(color="pink")))
-fg.add_child(folium.Marker(location=[64.74, -21.33], popup="I am a Marker-sama", icon=folium.Icon(color="pink")))
+for coordinates in [[64.14, -21.93], [64.34, -21.73], [64.54, -21.53], [64.74, -21.33]]:
+    fg.add_child(folium.Marker(location=coordinates, popup="I am a Marker", icon=folium.Icon(color="pink")))
+
 map.add_child(fg)
 
 map.save("Map1.html")
