e8e2f46835a4b3563461c3aebbf0c625f425036d
Ewelina Backtrog <oyfg@o2.pl>
2019-05-22 11:31:50 +0200

    add more marker-points

--- map1.py
+++ map1.py
@@ -2,7 +2,10 @@ import folium
 map = folium.Map(location=[64.14, -21.94], zoom_start=6, tiles="Mapbox Bright")
 
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location=[64.14, -21.93], popup="I am a Marker", icon=folium.Icon(color="pink")))
+fg.add_child(folium.Marker(location=[64.14, -21.93], popup="I am a Marker-kun", icon=folium.Icon(color="pink")))
+fg.add_child(folium.Marker(location=[64.34, -21.73], popup="I am a Marker-chan", icon=folium.Icon(color="pink")))
+fg.add_child(folium.Marker(location=[64.54, -21.53], popup="I am a Marker-san", icon=folium.Icon(color="pink")))
+fg.add_child(folium.Marker(location=[64.74, -21.33], popup="I am a Marker-sama", icon=folium.Icon(color="pink")))
 map.add_child(fg)
 
 map.save("Map1.html")
